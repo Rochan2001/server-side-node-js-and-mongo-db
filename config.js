5c74c9c708a1522651c21a6352269337e86a9f37
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2020-06-05 17:04:05 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
-}
+    secretKey: "12345-67890-09876-54321",
+    mongoUrl: "mongodb://localhost:27017/conFusion",
+    facebook: {
+        clientId: "695178077906809",
+        clientSecret: "050ef987c37f6da51b0d5d085d3f4c4c",
+    },
+};
